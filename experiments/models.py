96831546a320eecb3480cb503c89c226793515fd
Neha Srikanth <nehasrik@umd.edu>
2022-10-31 12:47:10 -0400

    refactoring modeling folder and adding defeasible trained model skeleton

--- experiments/models.py
+++ experiments/models.py
@@ -8,8 +8,6 @@ from itertools import chain
 
 class TrainedModel:
     
-    NUM_CHOICES = 2
-
     def __init__(self, trained_model_dir: str, cache_dir: str = 'checkpoints/hf_cache') -> None:
         self.tokenizer = TrainedModel.get_tokenizer(trained_model_dir, cache_dir)
         self.trained_model = TrainedModel.get_model(trained_model_dir, cache_dir)
@@ -17,6 +15,32 @@ class TrainedModel:
     @abstractmethod
     def predict(self) -> numpy.ndarray:
         raise NotImplementedError
+
+    @staticmethod
+    def get_tokenizer(model_path: str, hf_cache_path: str) -> AutoTokenizer:
+        return AutoTokenizer.from_pretrained(
+            model_path,
+            cache_dir=hf_cache_path,
+            use_fast=False,
+            revision="main",
+            use_auth_token=False,
+        )
+
+    @staticmethod
+    def get_model(model_path: str, hf_cache_path: str) -> AutoModelForMultipleChoice:
+        print('Loading model from %s' % model_path)
+        return AutoModelForMultipleChoice.from_pretrained(
+            model_path,
+            from_tf=bool(".ckpt" in model_path),
+            cache_dir=hf_cache_path
+        )
+
+
+class AbductiveTrainedModel(TrainedModel):
+    NUM_CHOICES = 2
+
+    def predict(self, obs1: str, obs2: str, hyp1: str, hyp2: str) -> numpy.ndarray:
+        return self._get_prediction(obs1, obs2, hyp1, hyp2)
 
     def _get_prediction(self, obs1, obs2, hyp1, hyp2) -> numpy.ndarray:
         """
@@ -48,21 +72,21 @@ class TrainedModel:
         probs = softmax(outputs.logits.detach().numpy(), axis=1)
         return probs[0]
 
-    @staticmethod
-    def get_tokenizer(model_path: str, hf_cache_path: str) -> AutoTokenizer:
-        return AutoTokenizer.from_pretrained(
-            model_path,
-            cache_dir=hf_cache_path,
-            use_fast=False,
-            revision="main",
-            use_auth_token=False,
-        )
 
-    @staticmethod
-    def get_model(model_path: str, hf_cache_path: str) -> AutoModelForMultipleChoice:
-        print('Loading model from %s' % model_path)
-        return AutoModelForMultipleChoice.from_pretrained(
-            model_path,
-            from_tf=bool(".ckpt" in model_path),
-            cache_dir=hf_cache_path
+class DefeasibleTrainedModel(TrainedModel):
+    NUM_CHOICES = 2
+
+    def predict(self, premise: str, hypothesis: str, update: str) -> numpy.ndarray:
+        return self._get_prediction((premise + hypothesis, update))
+
+    def _get_prediction(self, inp: Union[str, Tuple[str, str]]) -> numpy.ndarray:
+        result = self.tokenizer(
+            [inp],
+            padding="max_length", 
+            max_length=128, 
+            truncation=True, 
+            return_tensors="pt"
         )
+        outputs = self.trained_model(**result)
+        probs = softmax(outputs.logits.detach().numpy(), axis=1)
+        return probs[0]
