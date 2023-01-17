fdbcc1bea590d4e1e827504dd5ed4966488aa380
Neha Srikanth <nehasrik@umd.edu>
2023-01-16 18:51:02 -0800

    revamping and reorganizing consistency plot code

---
+++ experiments/human_consistency/defeasible/defeasible_consistency.py
@@ -0,0 +1,74 @@
+import json
+import pandas as pd
+import numpy as np
+import plotly.express as px
+
+import os
+import sys
+
+module_path = os.path.abspath(os.path.join('../../..'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
+    
+from annotated_data.annotated_data import dnli_dataset_by_name
+from utils import load_json, PROJECT_ROOT_DIR
+
+dnli_buckets = {k: {
+    'buckets': load_json(os.path.join(PROJECT_ROOT_DIR, f'modeling/roberta/defeasible/results/{k}_human.json'))
+} for k in dnli_dataset_by_name.keys()}
+
+
+def construct_bucket_metadata(buckets):
+    df = []
+    for ex_id, bucket in buckets.items():
+        confidences_in_correct_label = [
+            c['confidence'][bucket['gold_label']] for c in bucket['bucket_confidences']
+        ]
+        bucket_predictions = [c['prediction'] for c in bucket['bucket_confidences']]
+        bucket_consistency = len([x for x in bucket_predictions if x == bucket['original_prediction']])/len(bucket_predictions)
+        df.append({
+            'bucket_confidence_mean': np.mean(confidences_in_correct_label),
+            'bucket_confidence_var': np.var(confidences_in_correct_label),
+            'original_confidence': bucket['original_confidence'][bucket['gold_label']],
+            'bucket_consistency': bucket_consistency
+        })
+    return pd.DataFrame(df)
+
+
+def plot_orig_v_bucket_conf(df, plot_title):
+    fig = px.scatter(
+        df, 
+        x="original_confidence", 
+        y="bucket_confidence_mean", 
+        size="bucket_confidence_var",
+        title=plot_title,
+        trendline="ols",
+        color='bucket_consistency',
+        color_continuous_scale='Sunsetdark',
+        labels={
+         "original_confidence": "RoBERTa Confidence: Original Example",
+         "bucket_confidence_mean": "Mean Confidence: Paraphrased Examples",
+        }
+    )
+    fig.show()
+
+def plot_consistency_cdf(df, plot_title):
+    """
+    Empirical Cumulative Distribution Function (ECDF) plot:
+    rows of `data_frame` are sorted by the value `x`
+    and their cumulative count is drawn as a line.
+    """
+    fig = px.ecdf(
+        df, 
+        x="bucket_consistency", 
+        markers=True, 
+        marginal="histogram", 
+        color_discrete_sequence=['darkmagenta'],
+        title=plot_title,
+        labels={
+         "bucket_consistency": "Consistency (% of Bucket)",
+        }
+        
+    )
+    fig.update_layout(yaxis={"title": "% of Buckets with <= x Consistency"})
+    fig.show()
