15e9876bc5da6821bdf60c8dc5b71cd03b8b5010
Neha Srikanth <nehasrik@umd.edu>
2023-01-21 12:41:45 -0800

    adding object for atomic annotations

--- annotated_data/annotated_data.py
+++ annotated_data/annotated_data.py
@@ -40,9 +40,11 @@ def load_defeasible_paraphrased_set(validated_annotated_file: str) -> List[str]:
 
 dnli_social_human = load_defeasible_paraphrased_set(os.path.join(PROJECT_ROOT_DIR, 'annotated_data/defeasible/social/social_paraphrases_human.json'))
 dnli_snli_human = load_defeasible_paraphrased_set(os.path.join(PROJECT_ROOT_DIR, 'annotated_data/defeasible/snli/snli_paraphrases_human.json'))
+dnli_atomic_human = load_defeasible_paraphrased_set(os.path.join(PROJECT_ROOT_DIR, 'annotated_data/defeasible/atomic/atomic_paraphrases_human.json'))
 
 
 dnli_human_dataset_by_name = {
     'social': dnli_social_human,
-    'snli': dnli_snli_human
+    'snli': dnli_snli_human,
+    'atomic': dnli_atomic_human
 }
