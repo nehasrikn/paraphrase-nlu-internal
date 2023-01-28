8031c310830d0dcfecdda938d11a04135c56ee2a
Neha Srikanth <nehasrik@umd.edu>
2023-01-27 21:50:45 -0500

    plotting code for og conf shift plots

--- experiments/human_consistency/defeasible/defeasible_consistency.py
+++ experiments/human_consistency/defeasible/defeasible_consistency.py
@@ -2,6 +2,7 @@ import json
 import pandas as pd
 import numpy as np
 import plotly.express as px
+import plotly.graph_objects as go
 from scipy.special import logsumexp
 from scipy.spatial import distance
 from itertools import combinations
@@ -16,12 +17,19 @@ if module_path not in sys.path:
 from annotated_data.annotated_data import dnli_human_dataset_by_name
 from utils import load_json, PROJECT_ROOT_DIR
 
-dnli_human_buckets = {k: {
+dnli_human_bucket_predictions = {k: {
     'specialized_roberta': load_json(f'modeling/roberta/defeasible/results/{k}/{k}_human_d-{k}-roberta-large.json'),
     'unified_roberta': load_json(f'modeling/roberta/defeasible/results/{k}/{k}_human_dnli-roberta-large.json'),
     'specialized_full_input_lexical': load_json(f'modeling/fasttext/defeasible/results/{k}/{k}_human_d-{k}-full_input_lexical.json'),
     'specialized_partial_input_lexical': load_json(f'modeling/fasttext/defeasible/results/{k}/{k}_human_d-{k}-partial_input_lexical.json'),
     'gpt3-curie': load_json(f'modeling/gpt3/defeasible/results/{k}/{k}_human_gpt3-text-curie-001_processed.json')
+} for k in dnli_human_dataset_by_name.keys()}
+
+dnli_test_set_predictions = {k: {
+    'specialized_roberta': load_json(f'modeling/roberta/defeasible/results/{k}/{k}_test_set_d-{k}-roberta-large.json'),
+    'unified_roberta': load_json(f'modeling/roberta/defeasible/results/{k}/{k}_test_set_dnli-roberta-large.json'),
+    'specialized_full_input_lexical': load_json(f'modeling/fasttext/defeasible/results/{k}/{k}_test_set_d-{k}-full_input_lexical.json'),
+    'specialized_partial_input_lexical': load_json(f'modeling/fasttext/defeasible/results/{k}/{k}_test_set_d-{k}-partial_input_lexical.json'),
 } for k in dnli_human_dataset_by_name.keys()}
 
 def get_all_pairs_jensen_shannon_mean_distance(bucket_confidences):
@@ -43,15 +51,13 @@ def calculate_bucket_metadata(buckets):
         bucket_predictions = [c['prediction'] for c in bucket['bucket_confidences']]
         bucket_consistency = len([x for x in bucket_predictions if x == bucket['original_prediction']])/len(bucket_predictions)
 
-        #all_paraphrase_pairs_js_distance_mean = get_all_pairs_jensen_shannon_mean_distance(bucket['bucket_confidences'])
-
         metadata[ex_id] = {
             'bucket_confidence_mean': np.mean(confidences_in_correct_label),
             'bucket_confidence_var': np.var(confidences_in_correct_label),
             'original_confidence': bucket['original_confidence'][bucket['gold_label']],
             'bucket_consistency': bucket_consistency,
-            #'all_paraphrase_pairs_js_distance_mean': all_paraphrase_pairs_js_distance_mean,
-            #'mean_paraphrase_js_distance_from_original': get_mean_js_distance_from_original(bucket['bucket_confidences'], bucket['original_confidence'])
+            'conf_shift': np.mean(confidences_in_correct_label) - bucket['original_confidence'][bucket['gold_label']],
+            'orig_pred_shift': abs(bucket['original_confidence'][bucket['gold_label']] - 0.5)
         }
     
     return metadata
@@ -64,57 +70,36 @@ def construct_bucket_metadata(buckets):
 def get_original_example_prediction_accuracy(buckets, y_pred='original_prediction', y_true='gold_label'):
     return len([x for x in buckets.values() if x[y_pred] == x[y_true]])/len(buckets)
 
-def plot_mean_js_original_v_bucket_mean_distance(df, plot_title):
-    fig = px.scatter(
-        df, 
-        x="all_paraphrase_pairs_js_distance_mean", 
-        y="mean_paraphrase_js_distance_from_original", 
-        size="bucket_confidence_var",
-        title=plot_title,
-        trendline="ols",
-        color='bucket_consistency',
-        color_continuous_scale='Sunsetdark',
-        labels={
-         "original_confidence": "Model Confidence: Original Example",
-         "bucket_confidence_mean": "Mean Confidence: Paraphrased Examples",
-        }
-    )
-    fig.show()
-
 
 def plot_orig_v_bucket_conf(df, plot_title):
     fig = px.scatter(
         df, 
         x="original_confidence", 
-        y="bucket_confidence_mean", 
-        size="bucket_confidence_var",
+        y="conf_shift", 
         title=plot_title,
         trendline="ols",
         color='bucket_consistency',
-        color_continuous_scale='Sunsetdark',
+        size='bucket_confidence_var',
+        color_continuous_scale='Burg',
         labels={
          "original_confidence": "Model Confidence: Original Example",
-         "bucket_confidence_mean": "Mean Confidence: Paraphrased Examples",
+         "conf_shift": "Shift (Mean Bucket Conf - Original Conf)",
         }
     )
+    fig.update_traces(marker_sizemin=5, selector=dict(type='scatter'))
+
+    max_line = px.line(pd.DataFrame({'x': [0, 1], 'y': [1, 0]}), x="x", y="y")
+    max_line['data'][0]['line']['color']='rgb(0, 0, 0)'
+    fig['data'][0]['line']['width']=5
+
+    fig.add_trace(go.Scatter(x=[0,1], y=[1,0], name=None, line=dict(color='green', width=1, dash='dot')))
+    fig.add_trace(go.Scatter(x=[0,1], y=[0,-1], name=None, line=dict(color='green', width=1, dash='dot')))
+    fig.update(layout_showlegend=False)
+    fig.update_xaxes(range=[-0.1, 1.1])
+    fig.update_yaxes(range=[-1.1, 1.1])
+
     fig.show()
 
-def plot_orig_v_bucket_conf_js_consistency(df, plot_title):
-    fig = px.scatter(
-        df, 
-        x="original_confidence", 
-        y="bucket_confidence_mean", 
-        size="bucket_confidence_var",
-        title=plot_title,
-        trendline="ols",
-        color='mean_paraphrase_js_distance_from_original', #instead of bucket_consistency
-        color_continuous_scale='Sunsetdark',
-        labels={
-         "original_confidence": "Model Confidence: Original Example",
-         "bucket_confidence_mean": "Mean Confidence: Paraphrased Examples",
-        }
-    )
-    fig.show()
 
 def plot_consistency_cdf(df, plot_title):
     """
--- modeling/fasttext/defeasible/inference.py
+++ modeling/fasttext/defeasible/inference.py
@@ -1,5 +1,5 @@
 from modeling.fasttext.lexical_classifier import FastTextClassifier
-from defeasible_data import DefeasibleNLIExample, DefeasibleNLIDataset, ParaphrasedDefeasibleNLIExample
+from defeasible_data import DefeasibleNLIExample, DefeasibleNLIDataset, ParaphrasedDefeasibleNLIExample, dnli_datasets
 from typing import Union, List, Any, Dict
 from dataclasses import asdict
 from annotated_data.annotated_data import dnli_human_dataset_by_name
@@ -7,6 +7,7 @@ from utils import PROJECT_ROOT_DIR, write_json, load_json
 import os
 from tqdm import tqdm
 import string
+import random
 
 
 DNLI = "__label__{label} {premise} {hypothesis} {update}"
@@ -62,7 +63,22 @@ def bucket_predictions(
             'gold_label': paraphrases[0].original_example.label,
             'bucket_confidences': bucket_confidences,
         }
-    return buckets
+    return buckets, fasttext_classifier
+
+def test_set_evaluation(examples, fasttext_model):
+    predictions = []
+
+    for example in tqdm(examples):
+        prediction, confidence = fasttext_model.infer(example)
+
+        predictions.append({
+            'confidence': confidence.tolist(),
+            'prediction': prediction,
+            'label': example.label,
+            'example_id': example.example_id
+        })
+
+    return predictions
 
 
 if __name__ == '__main__':
@@ -71,21 +87,29 @@ if __name__ == '__main__':
             DefeasibleNLIExample(**e)
             for e in load_json(f'data_selection/defeasible/{dataset}/analysis_model_examples/train_examples.json')
         ]
+        random.seed(42)
+        analysis_models_training_examples = random.sample(analysis_models_training_examples, len(analysis_models_training_examples))
 
         ### Full input
-        buckets = bucket_predictions(
+        buckets, fasttext_model_full_input = bucket_predictions(
             analysis_models_training_examples, 
             evaluation_dataset, 
             os.path.join(PROJECT_ROOT_DIR, f'modeling/fasttext/defeasible/training_data/fasttext_train_full_input_{dataset}.txt'),
             partial_input=False
         )
-        write_json(buckets, os.path.join(PROJECT_ROOT_DIR, f'modeling/fasttext/defeasible/results/{dataset}/{dataset}_human_d-{dataset}-full_input_lexical.json'))
+        write_json(buckets, f'modeling/fasttext/defeasible/results/{dataset}/{dataset}_human_d-{dataset}-full_input_lexical.json')
+
+        test_set_preds_full_input = test_set_evaluation(dnli_datasets[dataset].test_examples, fasttext_model_full_input)
+        write_json(test_set_preds_full_input, f'modeling/fasttext/defeasible/results/{dataset}/{dataset}_test_set_d-{dataset}-full_input_lexical.json')
 
         ### Partial Input
-        buckets = bucket_predictions(
+        buckets, fasttext_model_partial_input = bucket_predictions(
             analysis_models_training_examples, 
             evaluation_dataset, 
             os.path.join(PROJECT_ROOT_DIR, f'modeling/fasttext/defeasible/training_data/fasttext_train_partial_input_{dataset}.txt'),
             partial_input=True
         )
-        write_json(buckets, os.path.join(PROJECT_ROOT_DIR, f'modeling/fasttext/defeasible/results/{dataset}/{dataset}_human_d-{dataset}-partial_input_lexical.json'))
+        write_json(buckets, f'modeling/fasttext/defeasible/results/{dataset}/{dataset}_human_d-{dataset}-partial_input_lexical.json')
+
+        test_set_preds_partial_input = test_set_evaluation(dnli_datasets[dataset].test_examples, fasttext_model_full_input)
+        write_json(test_set_preds_partial_input, f'modeling/fasttext/defeasible/results/{dataset}/{dataset}_test_set_d-{dataset}-partial_input_lexical.json')
