76c1a74b97120e95266d3b8ff670fc5d5aabfe2a
Neha Srikanth <nehasrik@umd.edu>
2023-12-02 17:11:09 -0500

    submission

--- plotting_utils.py
+++ plotting_utils.py
@@ -15,10 +15,10 @@ def plot_isocurves(ax: matplotlib.axes.Axes):
         min_pstay = 1 - 2*((1-pove)*total_variance) # 2 * UV, but max is when all variance is UV
         ax.plot(bernoullis, min_pstay, '--', alpha=0.3, color=palette[i])
         annotation_point = 500
-        curve_text = f"{int(100 * pove)}% POVE" if pove > 0 else "Min P(STAY)"
+        curve_text = f"{100 - int(100 * pove)}% PVAP" if pove > 0 else r"Min $P_C$"
         ax.annotate(
             curve_text, 
-            (bernoullis[annotation_point], min_pstay[annotation_point]+0.008), 
+            (bernoullis[annotation_point], min_pstay[annotation_point]+0.009), 
             rotation=0, 
             horizontalalignment='center', 
             verticalalignment='bottom', 
